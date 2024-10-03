0d5ecda597deea76e3c902353193d8d4743f3524
TJHarrington <tomhazz@gmail.com>
2024-10-03 15:48:50 +0100

    Initial styling changes and structure, plus CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,22 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Thomas James Harrington',
+			customCss: [
+				// Fontsource files for different font weights
+				'./node_modules/@fontsource/sora/400.css',
+				'./node_modules/@fontsource/sora/600.css',
+				'./node_modules/@fontsource/sora/800.css',
+				'./src/styles/custom.css'
+			],
+			logo: {
+				light: './src/assets/THLogoLight.svg',
+				dark: './src/assets/THLogoDark.svg',
+				replacesTitle: true,
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/t-j-harrington',
+				linkedin: ''
 			},
 			sidebar: [
 				{
@@ -15,6 +28,13 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'About me',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'CV', slug: 'about-me/cv' },
 					],
 				},
 				{
